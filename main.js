b5c90dd9cada6b36a024be55dae138ed0b5e3f1a
Konstantin Naumov <63791428+Garfeald@users.noreply.github.com>
2021-03-16 22:52:26 +0300

    add changed files

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 
-// const apiKey = '4dbc17e007ab436fb66416009dfb59a8'; #1
-const apiKey = '7878eaa2b917400fbc0b81e516b55e6b'
+// const apiKey = '4dbc17e007ab436fb66416009dfb59a8';
+// const apiKey = '7878eaa2b917400fbc0b81e516b55e6b';
+const apiKey = '295c6b46f60d41d18d38545d6281f15f';
 
 const conteiner = document.getElementById('conteiner');
 
-const buttonHideNews = document.getElementById('button-hide')
+const buttonHideNews = document.getElementById('button-hide');
 
 const data = new Date();
-let fixedDate = `${data.getDate()}.${data.getMonth() + 1}.${data.getFullYear()}`
 
-let statusCheckbox = document.getElementsByTagName('input')
+const fixedDate = `${data.getDate()}.${data.getMonth() + 1}.${data.getFullYear()}`;
 
-let statusText = document.getElementById('status__text')
-
-
-if(statusCheckbox.checked){
-		statusCheckbox.classList.add('status__text-active')
-	}
-  
+const readMessage = (id) => {
+    const statusText = document.getElementById(id)
+    statusText.classList.add('status__text-active');
+    document.getElementById(id).innerHTML = 'прочитано'
+    const messageCount = document.getElementById('messageCount').innerHTML
+    document.getElementById('messageCount').innerHTML = messageCount - 1
+} 
 
 function showNews() {
   fetch(`https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=${apiKey}`)
@@ -33,7 +33,7 @@ const renderContext = (response) => {
   const { articles } = response;
   let content = document.getElementById('conteiner').innerHTML
   let messageContent = document.getElementById('block-message').innerHTML
-  articles.map((dataFields) => {
+  articles.map((dataFields, index) => {
       content += 
       ` 
         <div class="news-block">
@@ -45,10 +45,9 @@ const renderContext = (response) => {
             <p><b>Author:</b> ${dataFields.author}</p>
             <div><b>Details: </b><a href="${dataFields.url}">ShowDetails</a></div>
             <p><b>Date:</b> ${fixedDate}</p>
-            <div class="status" >
-              <b>Status:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <input type="checkbox" id="status">
-              <p class="status__text" id="status__text">не прочитано</p>
+            <div class="status">
+              <b>Status:</b>
+              <p id="${index}" class="status__text" onclick="readMessage('${index}')">не прочитано</p>
             </div>
           </div>
         </div>
@@ -56,7 +55,7 @@ const renderContext = (response) => {
       `;
       messageContent =
       `
-        <div class="message"><b class="messageCount" >${articles.length}!!!</b></div>
+        <div class="message"><b class="messageCount" id="messageCount" >${articles.length}</b></div>
 
       `
       document.getElementById('block-message').innerHTML = messageContent;
